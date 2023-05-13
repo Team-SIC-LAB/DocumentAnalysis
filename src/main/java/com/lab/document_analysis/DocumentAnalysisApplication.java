14fc2f9eb4fafffb8fc0877ae930e969aef19ab6
Eunji Bang <eunji8106@naver.com>
2023-05-13 12:09:00 +0900

    first commit

---
+++ src/main/java/com/lab/document_analysis/DocumentAnalysisApplication.java
@@ -0,0 +1,13 @@
+package com.lab.document_analysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentAnalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentAnalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lab/document_analysis/DocumentAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.document_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocumentAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
